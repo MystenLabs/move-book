5b42010a5896833b0c59f38d0037cf59d78388f9
Damir Shamanaev <damirka.ru@gmail.com>
2024-03-20 00:21:21 +0300

    update 1710883281

--- temp.publish.sh
+++ temp.publish.sh
@@ -2,11 +2,11 @@
 
 time=$(date +%s)
 # echo "time: $time"
-git add .
-git commit -m "update $time"
-git checkout gh-pages
-mdbook build
-git rm -r sui
-mv book sui
-git add sui
-git commit -m "update $time"
+git add .;
+git commit -m "update $time";
+git checkout gh-pages;
+mdbook build;
+git rm -r sui;
+mv book sui;
+git add sui;
+git commit -m "update $time";
