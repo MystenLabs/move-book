2e7fb268a34d7d53a33e8cea2cc8f60af23e22bd
Damir Shamanaev <damirka.ru@gmail.com>
2024-03-20 00:20:40 +0300

    update 1710883240

---
+++ temp.publish.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+time=$(date +%s)
+# echo "time: $time"
+git add .
+git commit -m "update $time"
+git checkout gh-pages
+mdbook build
+git rm -r sui
+mv book sui
+git add sui
+git commit -m "update $time"
